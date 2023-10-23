c90be41588f149c4de80755bd84c415fe5cb1226
itouendou <145634271+itouendou@users.noreply.github.com>
2023-10-23 22:09:57 +0900

    add a file

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = Math.floor(Math.random()*10);
+
+console.log(num);
+
+if (num %3 === 0) {
+  console.log('3の倍数です');
+}
+if (num %5 === 0) {
+  console.log('5の倍数です');
+}
+if ((num %3 === 0)||(num %5 === 0)) {
+  console.log ('3と5の倍数です');
+} else {
+  console.log('条件が成り立ちませんでした');
+}
