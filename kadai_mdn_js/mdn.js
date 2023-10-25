d4ff6f3a58f4ecee01c86d3856eb38cfaa3b4876
saitou-ryou <145634271+saitou-ryou@users.noreply.github.com>
2023-10-25 20:55:43 +0900

    add a file

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const moonLanding = new Date ([2023,8,22]);
+
+let year = moonLanding.getFullYear();
+let month = moonLanding.getMonth() +1;
+let date = moonLanding.getDate();
+
+console.log(year + '年');
+console.log(month + '月');
+console.log(date + '日');
